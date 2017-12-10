020746976a3c424b5875fa0836886866ebdacfc3
JJ <jjyong666@gmail.com>
2017-12-10 12:24:08 -0500

    Add method to instantiate BeautifulSoup with offline web pages

--- parse.py
+++ parse.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import requests as requests
 
 web_url = 'http://www.ninjacuba.com/freelancers/fabian-ruiz-estevez'
+local_url = r"c:\profile2.html"
 
 
 def instantiate_soup_with_url(url):
@@ -11,7 +12,12 @@ def instantiate_soup_with_url(url):
     return new_soup
 
 
-soup = instantiate_soup_with_url(web_url)
+def instantiate_soup_with_local_page(url):
+    new_soup = BeautifulSoup(open(url).read(), "html.parser")
+    return new_soup
+
+
+soup = instantiate_soup_with_local_page(local_url)
 
 
 class HtmlParser(HTMLParser):
