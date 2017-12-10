fe373cebd00e878c8d35d0dfb61c522dbc28d5bc
JJ <jjyong666@gmail.com>
2017-12-10 12:20:06 -0500

    Import BeatifulSoup library to parse html files

--- parse.py
+++ parse.py
@@ -1,8 +1,17 @@
 from html.parser import HTMLParser
-
+from bs4 import BeautifulSoup
 import requests as requests
 
 web_url = 'http://www.ninjacuba.com/freelancers/fabian-ruiz-estevez'
+
+
+def instantiate_soup_with_url(url):
+    page = requests.get(url)
+    new_soup = BeautifulSoup(page, "html.parser")
+    return new_soup
+
+
+soup = instantiate_soup_with_url(web_url)
 
 
 class HtmlParser(HTMLParser):
